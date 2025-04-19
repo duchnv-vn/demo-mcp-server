c5cab3145772b1a0e0a2fae8db27eaf65983114b
duchnv-vn <huynhnguyenvietduc@gmail.com>
2025-04-20 00:42:02 +0700

    feat(ci): add build script

---
+++ nestjs-mcp-server/script/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+ROOT_DIR=$(pwd)
+
+source $ROOT_DIR/script/load-env.sh
+
+if [ $NODE_ENV == 'local' ]; then
+    nest buid
+fi
+
+if [ $NODE_ENV == 'dev' ]; then
+    nest buid
+fi
+
+if [ $NODE_ENV == 'prod' ]; then
+    nest buid
+fi
