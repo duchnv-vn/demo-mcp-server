04efeb754567f05c1d8bd093d33b86665f7c58ef
duchnv-vn <huynhnguyenvietduc@gmail.com>
2025-04-26 20:12:17 +0700

    feat(ci): modify docker files

--- nestjs-mcp-server/docker/prod/entrypoint.sh
+++ nestjs-mcp-server/docker/prod/entrypoint.sh
@@ -6,7 +6,7 @@ ls -a
 
 # Load environment variables from .env.prod file
 echo "Loading environment variables from .env.prod..."
-[ -f ./script/load-env.sh ] && source ./script/load-env.sh
+[ -f $ROOT_DIR/script/load-env.sh ] && source $ROOT_DIR/script/load-env.sh
 
 # Initialize database schema
 echo "Initializing database schema..."
