511662485d09ed13c0dcbc8cb26a8b8ff48fb9fa
duchnv-vn <huynhnguyenvietduc@gmail.com>
2025-04-26 22:13:40 +0700

    feat(db-access): log ip

--- nestjs-mcp-server/src/module/database-access-tool/database-access-tool.controller.ts
+++ nestjs-mcp-server/src/module/database-access-tool/database-access-tool.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Controller, Get, Ip, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { SSEServerTransport } from '@modelcontextprotocol/sdk/server/sse.js';
 import { DatabaseAccessToolService } from './database-access-tool.service';
@@ -12,7 +12,9 @@ export class DatabaseAccessToolController {
   ) {}
 
   @Get('sse')
-  connectSSE(@Res() res: Response) {
+  connectSSE(@Res() res: Response, @Ip() ip: string) {
+    console.log('=== CONNECT SSE ===', ip);
+
     try {
       this.transport = new SSEServerTransport(
         '/database-access-tools/messages',
@@ -26,7 +28,9 @@ export class DatabaseAccessToolController {
   }
 
   @Post('messages')
-  handleMessage(@Req() req: Request, @Res() res: Response) {
+  handleMessage(@Req() req: Request, @Res() res: Response, @Ip() ip: string) {
+    console.log('=== HANDLE MESSAGE ===', ip);
+
     try {
       if (this.transport) this.transport.handlePostMessage(req, res);
     } catch (error) {
